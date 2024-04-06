6ad416734442b60439078956c8d6fc04b3eb682b
gn045001 <gn045001@gmail.com>
2024-04-06 09:17:27 +0800

    整理上傳至mongoDB的問題

--- dockdata.sh
+++ dockdata.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #section 1:description 程式變數
 #= version: 0.1, date: 20240405, Creater: jiasian.lin
+#= version: 0.2, date: 20240406, Creater: jiasian.lin
 # This is the first section of the script. Set variables
 
 day=$(date +'%Y%-m%%-d')
-now=$(date +'%Y%m%d-%H%M%S')
+now=$(date +'%Y%m%d-%H%M')
 
 current_dir=$(pwd)
 directory="DackerData"
@@ -12,7 +13,9 @@ host=XXXXXX
 port=27017
 
 TOKEN="lbz6wRQ4qvbPQIPDQHTEiCMF2THiArWr8Utvjy0ZWG2"
-message=test
+disk_space=1073741824
+ThresholdMax=10000000
+ThresholdMin=5000000
 
 
 if [ ! -d "./log/" ]; then
@@ -22,13 +25,11 @@ fi
 
 disk_space=$(df -k . | awk 'NR==2 { print $4 }')
 
-message="可用空間僅剩下$(($disk_space / 1024)) MB"
+disk_message="可用空間僅剩下$(($disk_space / 1024)) MB"
 curl -X POST \
         -H "Authorization: Bearer $TOKEN" \
-        -F "message=$message" \
+        -F "message=$disk_message" \
         https://notify-api.line.me/api/notify
-
-
 
 echo -e "$(date) $(pwd)硬碟空間剩下$disk_space " >> "$current_dir/log/Summary.log"
 
@@ -94,13 +95,17 @@ else
         -F "message=$MESSAGE " \
         https://notify-api.line.me/api/notify
         # 將執行成功的訊息記錄到日誌中
-        echo -e "$(date) $MESSAGE Directory $current_dir/$directory already exists. Skipping creation." >> "$current_dir/$directory/log/Summary.log"  
-        
+        echo -e "$(date) $MESSAGE Directory $current_dir/$directory already exists. Skipping creation." >> "$current_dir/$directory/log/Summary.log"        
+    
+
+fi        
+
         #取得 硬碟剩餘空間
         df -h --output=source,size,used,avail,pcent,target | awk 'NR>1' | while read -r filesystem size used avail use_percent mounted_on; do
             echo "{ \"Filesystem\": \"$filesystem\", \"Size\": \"$size\", \"Used\": \"$used\", \"Available\": \"$avail\", \"Use%\": \"$use_percent\", \"Mounted on\": \"$mounted_on\" }" >> $current_dir/$directory/raw/"$now-DiskSpace.json"
-        done
+        done   
 
+        cd $current_dir/$directory
         #加入至DiskSpace mongodb 中
         tools/mongoimport --host "$host" --port $port --db admin --collection DiskSpace --file raw/"$now-DiskSpace.json" --username gn045001 --password gn045001
         #將Docker 相關資訊加入
@@ -109,8 +114,4 @@ else
         #加入至DockerState  mongodb 中
         tools/mongoimport --host "$host" --port $port --db admin --collection DockerState --file raw/"$now-DockerState.json" --username gn045001 --password gn045001
         mv "$now-DockerOutPut.json" raw/"$now-DockerOutPut.json"
-        
-
-fi
-
  echo -e "$(date) Start confirming the Server ." >> "$current_dir/$directory/log/Summary.log" 
